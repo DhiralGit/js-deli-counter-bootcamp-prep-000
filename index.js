b9a391d5a06f0aaf27a092e2c9327f56fa953d29
Dhiral Patel <dhiralpatel794@gmail.com>
2018-06-11 00:05:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var katzDeliLine = katzDeli.length;
 
 function takeANumber(katzDeliLine,name){
   katzDeli.push(name);
-  return "Welcome,"+name[katzDeliLine.length - 1]+". You are number";  
+  return "Welcome,"+name+". You are number";  
 };
