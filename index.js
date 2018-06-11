c1ee57f6cb214e0e5794d4df69198f1170408266
Dhiral Patel <dhiralpatel794@gmail.com>
2018-06-11 00:27:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function takeANumber(katzDeliLine,name){
 
 function nowServing() {
   if(katzDeliLine.length === 0 ) {
-    return "There is nobody waiting to be served!";
+    // return "There is nobody waiting to be served!";
   }
   else {
     var temp= katzDeliLine.shift();
